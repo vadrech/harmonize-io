d6fbed7c0facac39d6bca89a69c4f14b0274ed73
Charan Vadrevu <charan.vadrevu00@gmail.com>
2020-02-15 21:21:39 +0100

    changed layout for accessibility

--- main.py
+++ main.py
@@ -569,4 +569,5 @@ def HarmonizedMelody():
 app.logger.addHandler(logging.StreamHandler(sys.stdout))
 app.logger.setLevel(logging.ERROR)
     
-    
+if __name__=='__main__':
+    app.run(debug=True)
